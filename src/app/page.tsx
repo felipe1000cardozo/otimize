63385560b12ffa949631984c9ec1f082607f0aa5
felipe1000cardozo <felipe1000cardozo@hotmail.com>
2023-06-23 22:56:39 -0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <div >
+        <h1>
+          Otimizando...
+        </h1>
+        <h3>
+          testando deploy
+        </h3>
+      </div>
+    </main>
+  )
+}
